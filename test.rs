fef5986c1327e55501db5c18d034a7d588d7cbc1
Herman Skogseth <herman.skogseth@me.com>
2025-10-20 08:53:54 +0200

    With parameters

--- test.rs
+++ test.rs
@@ -1,28 +1,34 @@
 use libtest2::_private::{Case, DistributedList, DynCase};
 use libtest2::{RunResult, TestContext};
 
-static TESTS: DistributedList<DynCase> = DistributedList::root();
+static TESTS: DistributedList<DynCase<i32>> = DistributedList::root();
 
 fn main() {
     for (i, test) in TESTS.iter().enumerate() {
         println!("[{i}] {n}", n = test.name());
     }
 
-    libtest2::main(TESTS.iter().copied());
+    libtest2::main(TESTS.iter().copied(), 10);
 }
 
-#[libtest2::test]
-fn random_test(_context: &TestContext) -> RunResult {
-    assert_eq!(1 + 1, 2);
-    Ok(())
+libtest2::_private::test_parse! {
+    #[test i32]
+    fn random_test(_context: &TestContext<i32>) -> RunResult {
+        assert_eq!(1 + 1, 2);
+        Ok(())
+    }
 }
 
-#[libtest2::test]
-fn test_number_2(_context: &TestContext) -> RunResult {
-    Ok(())
+libtest2::_private::test_parse! {
+    #[test i32]
+    fn test_number_2(_context: &TestContext<i32>) -> RunResult {
+        Ok(())
+    }
 }
 
-#[libtest2::test]
-fn failing_test(_context: &TestContext) -> RunResult {
-    panic!("shit we failed boys");
+libtest2::_private::test_parse! {
+    #[test i32]
+    fn failing_test(_context: &TestContext<i32>) -> RunResult {
+        panic!("shit we failed boys");
+    }
 }
